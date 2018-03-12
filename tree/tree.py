807e78ce19c811ac03ea10e772514d2a6d36a0ec
baolinji <blj@xianjinxia.com>
2018-03-12 17:08:06 +0800

    add tree code

--- tree/tree.py
+++ tree/tree.py
@@ -10,27 +10,70 @@ class node(object):
         self.lchild = None
         self.rchild = None
 
-class Tree(node):
+class Tree():
 
-    def __init__(self,value):
-        super(Tree,self).__init__(value)
+    def __init__(self):
         self.root = None
 
-    def create(self,data):
-        cursor = self.root
-        if cursor is None:
-            cursor = node(data[0])
+    def create(self,t,data,i):
+        """先序创建"""
+        if data[i] =='#':
+            return None
+        else:
+            t = node(data[i])
+            t.lchild = self.create(t.lchild,data,i+1)
+            t.rchild = self.create(t.rchild,data,i+1)
+        return t
 
+    def add_node(self,data):
+        """层次"""
+        self.leaf = []
         for i in data:
-            #update tree
-            if cursor.lchild is None:
-                cursor.lchild = node(i)
-
+            if self.root == None:
+                self.root = node(i)
+                self.leaf.append(self.root)
             else:
-                cursor.rchild = node(i)
-                cursor = cursor.lchild
+                treenode = self.leaf[0]
+                if treenode.lchild is None:
+                    treenode.lchild = node(i)
+                    self.leaf.append(treenode.lchild)
+                else:
+                    treenode.rchild = node(i)
+                    self.leaf.append(treenode.rchild)
+                    self.leaf.pop(0)
 
-    def sTree(self,type=0):
+
+    def pre_visit(self,root):
+        if root is None:
+            return
+        else:
+            print root.value
+            self.pre_visit(root.lchild)
+            self.pre_visit(root.rchild)
+
+    def mid_visit(self,root):
+        if root is None:
+            return None
+        else:
+            self.mid_visit(root.lchild)
+            print root.value
+            self.mid_visit(root.rchild)
+
+    def aft_visit(self,root):
+        if root is None:
+            return
+        else:
+            self.aft_visit(root.rchild)
+            self.aft_visit(root.lchild)
+            print root.value
+
+
+    def insert(self,data):
+
+        pass
+
+
+    def sTree(self,target):
         """
         0:;
         1:;
@@ -40,9 +83,11 @@ class Tree(node):
         """
         if self.root is None:
             return
-        if type == 0:
+        if self.root.value> target:
             pass
-        if type == 1:
-            pass
-        if type == 3:
-            pass
+
+if __name__ == "__main__":
+    tree = Tree()
+    data = [1,2,3,4,5,6,'#',12,3,4,5,'#',12,4,3,4,'#']
+    tree.add_node(data)
+    print 1
